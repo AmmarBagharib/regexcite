5ff5483fdb2caa26f41cfd16e65aa7b9d108e058
AmmarBagharib <ammarbagharib@gmail.com>
2023-04-03 18:08:17 -0700

    add tests

---
+++ tests/testthat/test-my_add.R
@@ -0,0 +1,16 @@
+test_that("addition works", {
+  expect_equal(my_add(2, 4), 6)
+})
+
+test_that("function input", {
+  expect_error(my_add("10", "20"), "One of your inputs contains a string value")
+})
+
+test_that("null x input", {
+  expect_equal(is.na(my_add(NA)), TRUE)
+})
+
+test_that("null y input", {
+  expect_equal(is.na(my_add(10, NA)), TRUE)
+})
+
