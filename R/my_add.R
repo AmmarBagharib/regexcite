8cf29430d54fa7fc0ea10a6b4cb2f507cfa9fcd1
AmmarBagharib <ammarbagharib@gmail.com>
2023-04-03 18:19:55 -0700

    add roxygen skeleton

--- R/my_add.R
+++ R/my_add.R
@@ -1,3 +1,15 @@
+#' Addition of 2 inputts
+#'
+#' @param x first input
+#' @param y second input
+#'
+#' @return x + y
+#' @export
+#'
+#' @examples
+#' x <- 4
+#' y <- 3
+#' my_add(x, y)
 my_add <- function(x, y=10){
   if ((is.character(x) + is.character(y)) != 0){
     stop("One of your inputs contains a string value")
